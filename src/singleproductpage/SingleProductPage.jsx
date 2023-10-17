383996a2a2cb006d059cb8e86baed82b5d50dcc6
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-17 14:01:46 +0530

    changed class name only

--- src/singleproductpage/SingleProductPage.jsx
+++ src/singleproductpage/SingleProductPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const SingleProductPage = ({id, name, price, category}) => {
   return (
-    <div className='product-card'>
+    <div className='single-product-page'>
         <div>single product</div>
     </div>
   )
