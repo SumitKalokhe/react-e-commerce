9c5bef37830e23a6b0700466bd285c53a33d32a4
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-18 15:11:34 +0530

    used useParams, useContext hook and context API to get data for single product page

--- src/singleproductpage/SingleProductPage.jsx
+++ src/singleproductpage/SingleProductPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../components/context/AppContext'
 
-export const SingleProductPage = ({id, name, price, category}) => {
+
+const API = 'https://api.pujakaitem.com/api/products'
+export const SingleProductPage = () => {
+
+  const {getSingleProduct, singleProduct}= useContext(AppContext)
+  const { id } = useParams();
+  // console.log(singleProduct)
+  
+
+  useEffect(() => {
+    getSingleProduct(`${API}?id=${id}`)
+  },[id])
+
   return (
     <div className='single-product-page'>
-        <div>single product</div>
+      <div>{singleProduct.name}</div>
     </div>
   )
 }
