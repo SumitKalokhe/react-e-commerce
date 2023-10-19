6129d7c61d4c2b6157cfc93a2ba83fcd9171651e
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-19 10:36:53 +0530

    no important changes

--- src/singleproductpage/SingleProductPage.jsx
+++ src/singleproductpage/SingleProductPage.jsx
@@ -20,12 +20,14 @@ export const SingleProductPage = () => {
       <div className='single-product-image-div'>
         {productImage && <img className="single-product-image" src={productImage.url} alt="" />}
       </div>
-      <h1 className='single-product-name'>{singleProduct && singleProduct.name}</h1>
-      <p className='single-product-price'>MRP: {singleProduct && Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', maximumFractionDigits: 2 }).format(singleProduct.price / 100)}</p>
-      <p className='single-product-description'>{singleProduct && singleProduct.description}</p>
-      <p className='single-product-availability'>Available: {singleProduct && (singleProduct.stock > 0 ? "In Stock" : "Out of Stock")}</p>
-      <p>ID: {singleProduct&& singleProduct.id}</p>
-      <p>Brand: {singleProduct&& singleProduct.company}</p>
+      <div>
+        <h1 className='single-product-name'>{singleProduct && singleProduct.name}</h1>
+        <p className='single-product-price'>MRP: {singleProduct && Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', maximumFractionDigits: 2 }).format(singleProduct.price / 100)}</p>
+        <p className='single-product-description'>{singleProduct && singleProduct.description}</p>
+        <p className='single-product-availability'>Available: {singleProduct && (singleProduct.stock > 0 ? "In Stock" : "Out of Stock")}</p>
+        <p className='single-product-id'>ID: {singleProduct && singleProduct.id}</p>
+        <p className='single-product-brand'>Brand: {singleProduct && singleProduct.company}</p>
+      </div>
     </div>
   )
 }
