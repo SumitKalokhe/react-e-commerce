f7a61c1840f32119ff75e6c857eb169100861b04
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-21 12:39:06 +0530

    created filter for product category

---
+++ src/components/categoryfilter/CategoryFilter.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import './category-filter.css'
+import { AppContext } from '../context/AppContext'
+
+const CategoryFilter = () => {
+
+    const {filter, setFilter}= useContext(AppContext)
+
+    const putFilter=(categoryFilter)=>{
+
+        setFilter(categoryFilter)
+
+        return
+    }
+
+  return (
+    <div className='category-filter'>
+        <div onClick={()=>putFilter("all") }>All</div>
+        <div onClick={()=>putFilter("mobile") }>Mobile</div>
+        <div onClick={()=>putFilter("laptop") }>Laptop</div>
+        <div onClick={()=>putFilter("computer") }>Computer</div>
+        <div onClick={()=>putFilter("accessories") }>Accessories</div>
+        <div onClick={()=>putFilter("watch") }>Watch</div>
+    </div>
+  )
+}
+
+export default CategoryFilter
