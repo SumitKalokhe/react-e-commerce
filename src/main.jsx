4eb248162080d8c0fafaea936655c0fb6acc6fb9
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-21 19:29:12 +0530

    provided cart context provider to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AppContextProvider } from './components/context/AppContext.jsx'
+import { CartContextProvider } from './components/context/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </AppContextProvider>
   </React.StrictMode>,
 )
