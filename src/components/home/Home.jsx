71bdf35959874e08ae10ad1e3a5822a9782f4434
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-21 12:40:38 +0530

    added filter component on home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,17 +2,23 @@ import React, { useContext, useEffect } from 'react'
 import { AppContext } from '../context/AppContext'
 import { ProductPage } from '../../productpage/ProductPage'
 import './home.css'
+import CategoryFilter from '../categoryfilter/CategoryFilter'
 
 const Home = () => {
 
-  const {products}= useContext(AppContext)
-  
-  // console.log(products)
-  
+  const { products } = useContext(AppContext)
+
+
   return (
-    <div className='product-page max-width'>{products.map((items)=>{
-      return <ProductPage key={items.id} {...items}/>
-    })}</div>
+    <div className='home-page-layout'>
+      <div className='home-category-filter'>
+        <h4>Filters</h4>
+        <CategoryFilter />
+      </div>
+      <div className='product-page max-width'>{products.map((items) => {
+        return <ProductPage key={items.id} {...items} />
+      })}</div>
+    </div>
   )
 }
 
