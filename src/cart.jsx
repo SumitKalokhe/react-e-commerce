8f9cb2fb2f8e1fd9bc9b0f48d4a528ebbc66222c
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-21 19:30:18 +0530

    maodified cart.jsx which provides cart data from cart context to cart layout component

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from './components/context/CartContext'
+import CartLayout from './components/cartlayout/CartLayout';
+import './cart.css'
 
 export const Cart = () => {
+
+  const { cart } = useContext(CartContext)
+
+  console.log(cart);
+
   return (
-    <div>Cart</div>
+    <>
+      <div className='cart-table cart-layout-main' style={{marginTop: "3rem", fontSize:"1rem"}}>
+        <div className='cart-layout'>
+          <div className='sp-cart-layout'>
+            <div className='cart-table-name cart-product-name'>
+              <div>Name</div>
+            </div>
+            <div className='cart-table-quantity cart-product-quantity'>Quantity</div>
+            <div className='cart-table-price cart-product-price'>Price</div>
+          </div>
+        </div>
+
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem", margin: "1rem auto" }}>
+        {cart.map((items) => {
+          return (
+            <div className="cart-layout-main" key={items.id}>
+              <CartLayout item={items} />
+            </div>
+          );
+        })}
+      </div>
+    </>
   )
 }
