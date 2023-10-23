23c755b54cd3f42e996bd1337dc40f2cc09a4acc
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-10-23 16:42:40 +0530

    added remove from cart button and also functinality to not add same items in cart but just increase the quantity

--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -1,7 +1,6 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useEffect, useReducer } from 'react'
 
 export const CartContext = createContext();
-
 
 const initialState = {
     cart: [],
@@ -15,21 +14,64 @@ const reducer = (state, action) => {
     if (action.type === 'ADD_TO_CART') {
         let { id, name, price, quantity, singleProduct } = action.payload
 
-        let cartProduct = {
-            id,
-            name,
-            price,
-            quantity,
-            singleProduct
+        let existingProduct = state.cart.find((item) => item.id === id)
+
+        if (existingProduct) {
+            let updatedProduct = state.cart.map((item) => {
+                if (item.id === id) {
+
+                    let newQuantity = item.quantity + quantity
+
+                    if (newQuantity >= item.singleProduct.stock){
+                        newQuantity= item.singleProduct.stock
+                    }
+                    console.log(item.singleProduct.stock);
+
+                    return {
+                        ...item,
+                        quantity: newQuantity
+                    }
+                }
+
+                else {
+                    return item
+                }
+            })
+
+            return {
+                ...state,
+                cart: updatedProduct
+            }
+        } else {
+            let cartProduct = {
+                id,
+                name,
+                price,
+                quantity,
+                singleProduct
+            }
+
+            return {
+                ...state,
+                cart: [...state.cart, cartProduct]
+            }
         }
 
-        return {
-            ...state,
-            cart: [...state.cart, cartProduct]
-        }
+
 
 
     }
+    if (action.type === "REMOVE_CART_ITEM") {
+
+        let updatedCart = state.cart.filter((item) => item.id !== action.payload)
+
+        return {
+            ...state,
+            cart: updatedCart
+        }
+
+    }
+
     return state
 }
 
@@ -44,7 +86,11 @@ export const CartContextProvider = ({ children }) => {
 
     }
 
-    return <CartContext.Provider value={{ ...state, addToCart , cartItemTotal}}>
+    const removeCartItem = (id) => {
+        dispatch({ type: "REMOVE_CART_ITEM", payload: id })
+    }
+
+    return <CartContext.Provider value={{ ...state, addToCart, removeCartItem,cartItemTotal }}>
         {children}
     </CartContext.Provider>
 
